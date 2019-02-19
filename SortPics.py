66fa3ba551d0818ef5a2fcaf19f29ce8ce9b2db0
vigand <qvigand@gmail.com>
2019-02-19 13:55:17 +0000

    Adding informations display, success and failures

--- SortPics.py
+++ SortPics.py
@@ -12,7 +12,7 @@ def browse_button():
     global folder_path
     filename = filedialog.askdirectory()
     folder_path.set(filename)
-    print(filename)
+#    print(filename)
 
 class Win:
 
@@ -23,15 +23,21 @@ class Win:
         master.title("A simple GUI")
 
         self.folder = Label(master, textvariable=folder_path)
-        self.folder.pack()
         self.browse = Button(text="Browse", command=browse_button)
-        self.browse.pack()
-
         self.sortpic = Button(text="Sort pictures (PNG/JPEG)", command=self.sort_pictures)
-        self.sortpic.pack()
-
-
+        self.status = Label(root, text="Nothing wrong")
+        self.nb_err_jpg = Label(root, text="Number of non sorted jpg : 0")
+        self.nb_err_png = Label(root, text="Number of non sorted png : 0")
+        self.sorted = Label(root, text="Sorted files : 0")
         self.close_button = Button(master, text="Close", command=master.quit)
+
+        self.folder.pack()
+        self.browse.pack()
+        self.sortpic.pack()
+        self.status.pack()
+        self.nb_err_jpg.pack()
+        self.nb_err_png.pack()
+        self.sorted.pack()
         self.close_button.pack()
 
     def sort_pictures(self):
@@ -39,21 +45,41 @@ class Win:
         if (folder is ""):
             return
         onlyfiles = [f for f in listdir(folder) if isfile(join(folder, f))]
-        print(onlyfiles)
+#        print(onlyfiles)
+        sorted_files = 0
+        jpg_e = 0
+        png_e = 0
         for afile in onlyfiles:
             if afile.endswith((".jpg", ".jpeg", ".JPG", ".JPEG")):
+                tmp = join(folder, afile)
+#                print(tmp)
+                try:
+                    date = Image.open(tmp)._getexif()[36867]
+                    rename(tmp, join(folder, date.replace(' ', '_').replace(':', '-') + '.JPG'))
+                    sorted_files += 1
+                except Exception as e:
+                    print(e)
+                    print("No date for " + afile)
+                    jpg_e += 1
+                    self.status["text"] = "No date for " + afile
+
+# Since png are not standard, getexif while rarely, or never work.
+# TODO Still trying to find a clean native way to get the creation date from png files
+            elif afile.endswith((".PNG", ".png")):
                 tmp = join(folder, afile)
                 print(tmp)
                 try:
                     date = Image.open(tmp)._getexif()[36867]
-                    rename(tmp, join(folder, date.replace(' ', '_') + '.JPG'))
+                    rename(tmp, join(folder, date.replace(' ', '_') + '.PNG'))
+                    sorted_files += 1
                 except Exception as e:
                     print(e)
-                    print("no date for " + afile)
-                
-            elif afile.endswith((".PNG", ".png")):
-                pass
-                
+                    print("No date for " + afile)
+                    png_e += 1
+                    self.status["text"] = "No date for " + afile
+        self.nb_err_jpg["text"] = "Number of unchanged jpg : %d" % jpg_e
+        self.nb_err_png["text"] = "Number of unchanged png : %d" %  png_e
+        self.sorted["text"] = "Sorted files : %d" % sorted_files
 
 root = Tk()
 folder_path = StringVar()
